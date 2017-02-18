aebb1e834c422b5e959ea16fe0678f7d5d7f93db
janson <gandancing@gmail.com>
2017-02-18 17:32:42 +0800

    better for tests

--- jffs/koolshare/scripts/config_ss.sh
+++ jffs/koolshare/scripts/config_ss.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-#TODO better
-export PERP_BASE=/jffs/koolshare/perp
-export PATH=/jffs/koolshare/bin:/jffs/koolshare/scripts:/usr/bin:/sbin:/bin:/usr/sbin
+export KSROOT=/jffs/koolshare
+source $KSROOT/scripts/base.sh
 
-ACTION=$1
-ID=$1
-IP=127.0.0.1:80
+TEST=testssonly
+echo "$TEST"
+dbus set testss=$TEST
 
-http_response()  {
-        ARG0="$@"
-        curl -X POST -d "$ARG0" http://$IP/_resp/$ID
-}
-
-echo "test only"
-
-http_response "postend"
+http_response "$TEST"
