c7bc11cc482d9060282b91118b19bea7c807d3de
janson <gandancing@gmail.com>
2017-02-18 09:40:32 +0800

    add softcenter for tt

---
+++ softcenter/app.json.js
@@ -0,0 +1,128 @@
+{
+    "apps": [
+        {
+            "build_date": "2016-05-19_22:37:04", 
+            "description": "中文SSID，装逼利器！", 
+            "home_url": "Module_ssid.asp", 
+            "md5": "fe117409dde27578229650f93465efaf", 
+            "name": "ssid", 
+            "tar_url": "ssid/ssid.tar.gz", 
+            "title": "中文SSID", 
+            "version": "1.3"
+        }, 
+        {
+            "build_date": "2017-02-06_22:20:04", 
+            "description": "", 
+            "home_url": "Module_policy_route.asp", 
+            "md5": "81a8f99e947851378fa5b24914cf5ecd", 
+            "name": "dualwan", 
+            "tar_url": "dualwan/dualwan.tar.gz", 
+            "title": "策略路由", 
+            "version": "1.6"
+        }, 
+        {
+            "build_date": "2016-04-27_23:31:52", 
+            "description": "让测速更简单", 
+            "home_url": "Module_speedtest.asp", 
+            "md5": "5a072a57af3b9bb1343a241a44278dd2", 
+            "name": "speedtest", 
+            "tar_url": "speedtest/speedtest.tar.gz", 
+            "title": "网络测速", 
+            "version": "0.2.2"
+        }, 
+        {
+            "build_date": "2016-11-25_21:41:36", 
+            "description": "SVPN让游戏更畅快", 
+            "home_url": "Module_shadowvpn.asp", 
+            "md5": "2f156357fb324562eda66410a6b2ad64", 
+            "name": "shadowvpn", 
+            "tar_url": "shadowvpn/shadowvpn.tar.gz", 
+            "title": "Shadowvpn", 
+            "version": "2.9"
+        }, 
+        {
+            "build_date": "2017-01-15_19:54:28", 
+            "description": "使用Dnspod的ddns服务", 
+            "home_url": "Module_ddnspod.asp", 
+            "md5": "f760ab7b12017e07f5668d88c7f607ae", 
+            "name": "ddnspod", 
+            "tar_url": "ddnspod/ddnspod.tar.gz", 
+            "title": "DDnspod", 
+            "version": "0.1.6"
+        }, 
+        {
+            "build_date": "2016-10-05_16:51:29", 
+            "description": "超强的SSH网页客户端，webshell替代品~", 
+            "home_url": "Module_shellinabox.asp", 
+            "md5": "9597d03693b8bf8ee7e6d3d565eed5a5", 
+            "name": "shellinabox", 
+            "tar_url": "shellinabox/shellinabox.tar.gz", 
+            "title": "shellinabox", 
+            "version": "1.4"
+        }, 
+        {
+            "build_date": "2016-10-09_14:54:55", 
+            "description": "老板，来一斤虚拟内存~", 
+            "home_url": "Module_swap.asp", 
+            "md5": "d7e8e92a0454dd57f1a31681c912a0f5", 
+            "name": "swap", 
+            "tar_url": "swap/swap.tar.gz", 
+            "title": "虚拟内存", 
+            "version": "2.2"
+        }, 
+        {
+            "build_date": "2016-11-22_21:53:42", 
+            "description": "基于kcp协议的udp网络加速", 
+            "home_url": "Module_kcptun.asp", 
+            "md5": "7f103db8cdb9cd8a69a1d871006a6d3a", 
+            "name": "kcptun", 
+            "tar_url": "kcptun/kcptun.tar.gz", 
+            "title": "kcptun", 
+            "version": "2.2.6"
+        }, 
+        {
+            "build_date": "2016-06-20_17:00:29", 
+            "description": "迅雷快鸟，为上网加速而生~", 
+            "home_url": "Module_kuainiao.asp", 
+            "md5": "9bb032598894d2a7f4f7356127e725a9", 
+            "name": "kuainiao", 
+            "tar_url": "kuainiao/kuainiao.tar.gz", 
+            "title": "讯雷快鸟", 
+            "version": "0.3.6"
+        }, 
+        {
+            "build_date": "2016-06-29_11:33:47", 
+            "description": "让局域网控制能简单", 
+            "home_url": "", 
+            "md5": "3631291ffa400f09c57b0b8a789e1161", 
+            "name": "phddns", 
+            "tar_url": "phddns/phddns.tar.gz", 
+            "title": "花生壳内网版", 
+            "version": "0.2"
+        }, 
+        {
+            "build_date": "2017-02-17_00:15:45", 
+            "description": "科学上网", 
+            "home_url": "Main_Ss_Content.asp", 
+            "md5": "42edc029ec547e68bfc0d847f75bbb1d", 
+            "name": "shadowsocks", 
+            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
+            "title": "shadowsocks", 
+            "version": "3.3.3"
+        }, 
+        {
+            "build_date": "2016-05-07_01:41:30", 
+            "description": "点对点建立连接,下载更快", 
+            "home_url": "", 
+            "md5": "21f65a490064f311481d3db3c5068a9d", 
+            "name": "koolnet", 
+            "tar_url": "koolnet/koolnet.tar.gz", 
+            "title": "P2P 穿透", 
+            "version": "0.2"
+        }
+    ], 
+    "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
+    "md5": "dc3e6e9cefa2defbb845b2e4cbf43337", 
+    "tar_url": "softcenter/softcenter.tar.gz", 
+    "version": "1.2.9"
+}
---
+++ softcenter/config.json.js
@@ -0,0 +1,4 @@
+{
+"version":"1.2.9",
+"md5":"dc3e6e9cefa2defbb845b2e4cbf43337"
+}
