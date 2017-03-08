e38afdcf66f3792b618d60240d26f19b0365c1c0
Major <Major-k@users.noreply.github.com>
2017-03-08 13:35:21 +0800

    Update build.sh

--- aliddns/build.sh
+++ aliddns/build.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-tar zcvf aliddns.tar.gz aliddns
+MODULE=aliddns
+VERSION=0.4
+TITLE=阿里云DDNS
+DESCRIPTION=阿里云解析自动更新IP
+HOME_URL=Module_aliddns.asp
+
+# Check and include base
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ "$MODULE" == "" ]; then
+	echo "module not found"
+	exit 1
+fi
+
+if [ -f "$DIR/$MODULE/$MODULE/install.sh" ]; then
+	echo "install script not found"
+	exit 2
+fi
+
+# now include build_base.sh
+. $DIR/../softcenter/build_base.sh
+
+# change to module directory
+cd $DIR
+
+# do something here
+
+do_build_result
