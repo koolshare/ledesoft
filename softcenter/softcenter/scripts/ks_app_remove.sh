bc91262f07151adebf531c594d2953d0c616c6b2
Major <Major-k@users.noreply.github.com>
2017-03-12 03:43:10 +0800

    Update ks_app_remove.sh

--- softcenter/softcenter/scripts/ks_app_remove.sh
+++ softcenter/softcenter/scripts/ks_app_remove.sh
@@ -6,31 +6,6 @@ source $KSROOT/scripts/base.sh
 #From dbus to local variable
 eval `dbus export softcenter_installing_`
 
-#softcenter_installing_module 	#正在安装的模块
-#softcenter_installing_todo 	#希望安装的模块
-#softcenter_installing_tick 	#上次安装开始的时间
-#softcenter_installing_version 	#正在安装的版本
-#softcenter_installing_md5 	#正在安装的版本的md5值
-#softcenter_installing_tar_url 	#模块对应的下载地址
-
-#softcenter_installing_status=		#尚未安装
-#softcenter_installing_status=0		#尚未安装
-#softcenter_installing_status=1		#已安装
-#softcenter_installing_status=2		#将被安装到jffs分区...
-#softcenter_installing_status=3		#正在下载中...请耐心等待...
-#softcenter_installing_status=4		#正在安装中...
-#softcenter_installing_status=5		#安装成功！请5秒后刷新本页面！...
-#softcenter_installing_status=6		#卸载中......
-#softcenter_installing_status=7		#卸载成功！
-#softcenter_installing_status=8		#没有检测到在线版本号！
-#softcenter_installing_status=9		#正在下载更新......
-#softcenter_installing_status=10	#正在安装更新...
-#softcenter_installing_status=11	#安装更新成功，5秒后刷新本页！
-#softcenter_installing_status=12	#下载文件校验不一致！
-#softcenter_installing_status=13	#然而并没有更新！
-#softcenter_installing_status=14	#正在检查是否有更新~
-#softcenter_installing_status=15	#检测更新错误！
-
 softcenter_home_url=`dbus get softcenter_home_url`
 CURR_TICK=`date +%s`
 BIN_NAME=$(basename "$0")
@@ -47,8 +22,9 @@ UNINSTALL_SUFFIX=_uninstall
 
 LOGGER() {
 #	echo $1
-	logger $1
 	http_response $1
+	logger $1
+	
 }
 
 uninstall_module() {
@@ -57,19 +33,19 @@ uninstall_module() {
 	fi
 	LAST_TICK=`expr $softcenter_installing_tick + 20`
 	if [ "$LAST_TICK" -ge "$CURR_TICK" -a "$softcenter_installing_module" != "" ]; then
-		LOGGER "2" #"module $softcenter_installing_module is installing"
+		LOGGER "<font color=red>插件 $softcenter_installing_module 正在安装中</font>"
 		exit 2
 	fi
 
 	if [ "$softcenter_installing_todo" = "" -o "$softcenter_installing_todo" = "softcenter" ]; then
 		#curr module name not found
-		LOGGER "3" #"module name not found"
+		LOGGER "<font color=red>不存在的插件</font>"
 		exit 3
 	fi
 
 	ENABLED=`dbus get "$softcenter_installing_todo""_enable"`
 	if [ "$ENABLED" = "1" ]; then
-		LOGGER "9" #"please disable this module than try again"
+		LOGGER "<font color=red>程序正在运行，请关闭后再试！</font>"
 		exit 4
 	fi
 
