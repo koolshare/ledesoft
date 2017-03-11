9701a83ebb33c55edabd824a0c9c51ffec9ec749
Major <Major-k@users.noreply.github.com>
2017-03-12 05:06:29 +0800

    Update aliddns_config.sh

--- aliddns/aliddns/scripts/aliddns_config.sh
+++ aliddns/aliddns/scripts/aliddns_config.sh
@@ -6,11 +6,9 @@ if [ "`dbus get aliddns_enable`" = "1" ]; then
     cru d aliddns
     cru a aliddns "*/`dbus get aliddns_interval` * * * * /bin/sh $KSROOT/scripts/aliddns_update.sh"
     # run once after submit
-	sleep 2
 	sh $KSROOT/scripts/aliddns_update.sh
 	sleep 1
-	http_response '设置已保存！切勿重复提交！页面将在3秒后刷新'
-
 else
     cru d aliddns
 fi
+http_response '设置已保存！切勿重复提交！页面将在3秒后刷新'
