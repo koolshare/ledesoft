29d880ce76606b288ab6c9ae30de8b6abb668842
Major <Major-k@users.noreply.github.com>
2017-03-11 15:24:18 +0800

    Update aliddns_config.sh

--- aliddns/aliddns/scripts/aliddns_config.sh
+++ aliddns/aliddns/scripts/aliddns_config.sh
@@ -2,11 +2,16 @@
 
 export KSROOT=/jffs/koolshare
 source $KSROOT/scripts/base.sh
-http_response '设置已保存，请2分钟后查看状态！切勿重复提交！页面将在10秒后刷新'
+
 if [ "`dbus get aliddns_enable`" = "1" ]; then
     dbus delay aliddns_timer `dbus get aliddns_interval` $KSROOT/scripts/aliddns_update.sh
+    # run once after submit
+	sleep 2
+	sh $KSROOT/scripts/aliddns_update.sh
+	sleep 1
+	http_response '设置已保存！切勿重复提交！页面将在10秒后刷新'
 
 else
     dbus remove __delay__aliddns_timer
-
+    nvram set ddns_hostname_x=`nvram get ddns_hostname_old`
 fi
