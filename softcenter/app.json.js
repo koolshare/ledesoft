4962714b81faf1bd20cf51b5e226cb62accd9380
lixin <sadoneli@gmail.com>
2017-03-15 00:06:25 +0800

    update softcenter

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -11,41 +11,6 @@
             "version": "0.4"
         }, 
         {
-            "build_date": "2017-01-15_19:54:28", 
-            "description": "使用Dnspod的ddns服务", 
-            "home_url": "Module_ddnspod.asp", 
-            "md5": "f760ab7b12017e07f5668d88c7f607ae", 
-            "name": "ddnspod", 
-            "tar_url": "ddnspod/ddnspod.tar.gz", 
-            "title": "DDnspod", 
-            "version": "0.1.6"
-        }, 
-        {
-            "name": "shadowsocks", 
-            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
-            "title": "shadowsocks"
-        }, 
-        {
-            "build_date": "2016-06-29_11:33:47", 
-            "description": "让局域网控制能简单", 
-            "home_url": "", 
-            "md5": "3631291ffa400f09c57b0b8a789e1161", 
-            "name": "phddns", 
-            "tar_url": "phddns/phddns.tar.gz", 
-            "title": "花生壳内网版", 
-            "version": "0.2"
-        }, 
-        {
-            "build_date": "2016-06-20_17:00:29", 
-            "description": "迅雷快鸟，为上网加速而生~", 
-            "home_url": "Module_kuainiao.asp", 
-            "md5": "9bb032598894d2a7f4f7356127e725a9", 
-            "name": "kuainiao", 
-            "tar_url": "kuainiao/kuainiao.tar.gz", 
-            "title": "讯雷快鸟", 
-            "version": "0.3.6"
-        }, 
-        {
             "build_date": "2016-12-27_09:15:58", 
             "description": "koolproxy", 
             "home_url": "Module_koolproxy.asp", 
@@ -68,7 +33,7 @@
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
-    "md5": "de47b9dc177d496576405a3f12c64296", 
+    "md5": "179320bbb0099468f2460fa64d1730ff", 
     "tar_url": "softcenter/softcenter.tar.gz", 
     "version": "1.3.2"
 }
