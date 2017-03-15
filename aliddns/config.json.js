ae985367e2cb0acd4986c548136b038d2aac3c35
Major <Major-k@users.noreply.github.com>
2017-03-15 21:47:34 +0800

    Update config.json.js

--- aliddns/config.json.js
+++ aliddns/config.json.js
@@ -2,7 +2,7 @@
 "version":"0.4",
 "md5":"610d41503b2fbf80cd4e98755b7d5df2",
 "home_url":"Module_aliddns.asp",
-"title":"阿里云DDNS",
-"description":"阿里云DDNS动态域名解析",
+"title":"AliDDNS",
+"description":"阿里云DDNS域名解析",
 "build_date":"2017-03-10_08:56:14"
 }
