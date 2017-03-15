869cf53ad0bc25577e837c37f74f2359c3fbca59
Major <Major-k@users.noreply.github.com>
2017-03-15 21:46:08 +0800

    Update config.json.js

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,9 +1,9 @@
 {
     "build_date": "2016-12-27_09:15:58", 
-    "description": "koolproxy", 
+    "description": "去屑不油腻", 
     "home_url": "Module_koolproxy.asp", 
     "md5": "7f69b168fd85b5b6f16cdf91770d233d", 
     "module": "koolproxy", 
-    "title": "koolproxy", 
+    "title": "KoolProxy", 
     "version": "3.3.4"
 }
