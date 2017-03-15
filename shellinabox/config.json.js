892e707673d47191263cd942d4cd0b9291f8303b
Major <Major-k@users.noreply.github.com>
2017-03-15 21:48:25 +0800

    Update config.json.js

--- shellinabox/config.json.js
+++ shellinabox/config.json.js
@@ -2,7 +2,7 @@
 "version":"1.6",
 "md5":"0dffac6ddc19c44a87208f77c923b54f",
 "home_url":"Module_shellinabox.asp",
-"title":"shellinabox",
-"description":"超强的SSH网页客户端，webshell替代品~",
+"title":"ShellnaBox",
+"description":"超强的SSH网页客户端",
 "build_date":"2017-03-15_08:42:22"
 }
