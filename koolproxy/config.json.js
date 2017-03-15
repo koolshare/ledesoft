da06671b6a5fc10efec09fbe9ff22a0a4e3fa8af
lixin <sadoneli@gmail.com>
2017-03-15 09:46:59 +0800

    fix koolproxy title

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "koolproxy", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "0b01fcae8b72f9498c1d87fe3047de5b", 
+    "md5": "7f69b168fd85b5b6f16cdf91770d233d", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.3.4"
