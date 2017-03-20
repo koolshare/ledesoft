79155315303cb9ab7066c5e15c8d10fda7f93f02
JSMonkey <Major-k@users.noreply.github.com>
2017-03-21 01:39:41 +0800

    Update shellinabox_start.sh

--- shellinabox/shellinabox/shellinabox/shellinabox_start.sh
+++ shellinabox/shellinabox/shellinabox/shellinabox_start.sh
@@ -1,4 +1,18 @@
 #! /bin/sh
 
-#start shellinaboxd
-$KSROOT/shellinabox/shellinaboxd --css=/jffs/koolshare/shellinabox/white-on-black.css -b
+if [ ! -L "$KSROOT/init.d/S99Shellinabox.sh" ]; then 
+	ln -sf $KSROOT/shellinabox/shellinabox_start.sh $KSROOT/init.d/S99Shellinabox.sh
+fi
+case $ACTION in
+start)
+	killall shellinaboxd
+	$KSROOT/shellinabox/shellinaboxd --css=/jffs/koolshare/shellinabox/white-on-black.css -b
+	;;
+stop)
+	killall shellinaboxd
+	;;
+*)
+	killall shellinaboxd
+	$KSROOT/shellinabox/shellinaboxd --css=/jffs/koolshare/shellinabox/white-on-black.css -b
+	;;
+esac
