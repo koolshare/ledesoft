6dfc9e18c3627884f1f880dbae6e8e246390ce6a
JSMonkey <Major-k@users.noreply.github.com>
2017-03-20 15:37:04 +0800

    Create config.json.js

---
+++ kms/config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-11-25_21:35:44", 
+    "description": "不可描述", 
+    "home_url": "Module_kms.asp", 
+    "md5": "2cbe66855c13d24e3d81d6d9093f99bc", 
+    "module": "kms", 
+    "title": "系统工具", 
+    "version": "1.3"
+}
