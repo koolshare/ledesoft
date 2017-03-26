4cfd3cbfbeb94a9687dfcf8c941b61aefd3494a7
lixin <sadoneli@gmail.com>
2017-03-26 21:20:19 +0800

    update software center#2

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,6 @@
 {
 "title":"<b>Tomato&nbsp;-&nbsp;软件中心</b>",
-"content1":"<b>notice:tomato software center is based on advanced tomato!</b>",
-"content2":"插件中心正处于开发当中，在使用过程中出现任何问题请至论坛反馈。",
-"content3":"从0.1.5版本开始，因为更换skipd了数据库，需要清空jffs分区后重装软件中心才能正常使用！",
-"content4":"tomato软件中心在正式发布前，新的软件版本可能出现不兼容老版本，因此出现问题可以尝试清空jffs分区重装软件中心。"
+"content1":"插件中心正处于开发当中，在使用过程中出现任何问题请至论坛反馈。",
+"content2":"从0.1.5版本开始，因为更换skipd了数据库，需要清空jffs分区后重装软件中心才能正常使用！",
+"content3":"tomato软件中心在正式发布前，新的软件版本可能出现不兼容老版本，因此出现问题可以尝试清空jffs分区重装软件中心。"
 }
