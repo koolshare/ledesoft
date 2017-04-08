0a0435f98ea6457bf7d336dafdcf933a23e7823e
janson <gandancing@gmail.com>
2017-04-08 15:14:26 +0800

    update softcenter to 0.2.5

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,6 +1,21 @@
 {
     "apps": [
         {
+            "build_date": "2017-03-22_19:23:25", 
+            "description": "shell on web~", 
+            "home_url": "Module_webshell.asp", 
+            "md5": "3ccc1719f5deb785e7a1628a44932a65", 
+            "name": "webshell", 
+            "tar_url": "webshell/webshell.tar.gz", 
+            "title": "webshell", 
+            "version": "1.0.2"
+        }, 
+        {
+            "name": "phddns", 
+            "tar_url": "phddns/phddns.tar.gz", 
+            "title": "phddns"
+        }, 
+        {
             "build_date": "2017-03-24_02:59:40", 
             "description": "阿里云解析自动更新IP", 
             "home_url": "Module_aliddns.asp", 
@@ -11,14 +26,24 @@
             "version": "0.7"
         }, 
         {
-            "build_date": "2017-03-27_09:47:47", 
-            "description": "虚拟内存~", 
-            "home_url": "Module_swap.asp", 
-            "md5": "6033a791d4368240c54946d1d27dcfed", 
-            "name": "swap", 
-            "tar_url": "swap/swap.tar.gz", 
-            "title": "swap", 
-            "version": "1.0.3"
+            "build_date": "2017-04-01_09:08:23", 
+            "description": "fuck gfw", 
+            "home_url": "Module_shadowsocks.asp", 
+            "md5": "0d6ddf45c882f444ce6c382e870e2e25", 
+            "name": "shadowsocks", 
+            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
+            "title": "shadowsocks", 
+            "version": "1.1.7"
+        }, 
+        {
+            "build_date": "2017-03-21_01:42:53", 
+            "description": "超强的SSH网页客户端", 
+            "home_url": "Module_shellinabox.asp", 
+            "md5": "49ff206d849dfe50f72d3323bbb8af8d", 
+            "name": "shellinabox", 
+            "tar_url": "shellinabox/shellinabox.tar.gz", 
+            "title": "ShellnaBox", 
+            "version": "1.8"
         }, 
         {
             "build_date": "2016-11-25_21:35:44", 
@@ -32,24 +57,14 @@
             "version": "1.3"
         }, 
         {
-            "build_date": "2017-04-01_09:08:23", 
-            "description": "fuck gfw", 
-            "home_url": "Module_shadowsocks.asp", 
-            "md5": "0d6ddf45c882f444ce6c382e870e2e25", 
-            "name": "shadowsocks", 
-            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
-            "title": "shadowsocks", 
-            "version": "1.1.7"
-        }, 
-        {
-            "build_date": "2017-03-22_19:24:37", 
-            "description": "softether_vpn", 
-            "home_url": "Module_softether_vpn.asp", 
-            "md5": "ca50a5ed0db3087cc44b5d4e9fd75e4b", 
-            "name": "softether_vpn", 
-            "tar_url": "softether_vpn/softether_vpn.tar.gz", 
-            "title": "softether_vpn", 
-            "version": "1.0.1"
+            "build_date": "2017-03-27_09:47:47", 
+            "description": "虚拟内存~", 
+            "home_url": "Module_swap.asp", 
+            "md5": "6033a791d4368240c54946d1d27dcfed", 
+            "name": "swap", 
+            "tar_url": "swap/swap.tar.gz", 
+            "title": "swap", 
+            "version": "1.0.3"
         }, 
         {
             "build_date": "2017-03-29_18:38:37", 
@@ -62,28 +77,18 @@
             "version": "3.3.6"
         }, 
         {
-            "build_date": "2017-03-22_19:23:25", 
-            "description": "shell on web~", 
-            "home_url": "Module_webshell.asp", 
-            "md5": "3ccc1719f5deb785e7a1628a44932a65", 
-            "name": "webshell", 
-            "tar_url": "webshell/webshell.tar.gz", 
-            "title": "webshell", 
-            "version": "1.0.2"
-        }, 
-        {
-            "build_date": "2017-03-21_01:42:53", 
-            "description": "超强的SSH网页客户端", 
-            "home_url": "Module_shellinabox.asp", 
-            "md5": "49ff206d849dfe50f72d3323bbb8af8d", 
-            "name": "shellinabox", 
-            "tar_url": "shellinabox/shellinabox.tar.gz", 
-            "title": "ShellnaBox", 
-            "version": "1.8"
+            "build_date": "2017-03-22_19:24:37", 
+            "description": "softether_vpn", 
+            "home_url": "Module_softether_vpn.asp", 
+            "md5": "ca50a5ed0db3087cc44b5d4e9fd75e4b", 
+            "name": "softether_vpn", 
+            "tar_url": "softether_vpn/softether_vpn.tar.gz", 
+            "title": "softether_vpn", 
+            "version": "1.0.1"
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
-    "md5": "87d02394a36545aa1febb429f45c0565", 
+    "md5": "4770da9a435bcaf938383550b03e4b4f", 
     "tar_url": "softcenter/softcenter.tar.gz", 
-    "version": "0.2.4"
+    "version": "0.2.5"
 }
--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,6 +1,6 @@
 {
 "title":"<b>Tomato&nbsp;-&nbsp;软件中心</b>",
 "content1":"插件中心正处于开发当中，在使用过程中出现任何问题请至论坛反馈。",
-"content2":"从0.1.5版本开始，因为更换skipd了数据库，需要清空jffs分区后重装软件中心才能正常使用！",
+"content2":"从0.2.5版本开始，因为更换skipd了数据库，需要清空jffs分区后重装软件中心才能正常使用！手动备份方法：dbus listall > /jffs/backup.txt; 手动导入方法：dbus import /jffs/backup.txt",
 "content3":"tomato软件中心在正式发布前，新的软件版本可能出现不兼容老版本，因此出现问题可以尝试清空jffs分区重装软件中心。"
 }
