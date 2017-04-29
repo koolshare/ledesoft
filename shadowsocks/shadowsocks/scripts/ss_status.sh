7d716f5330bc27e34fd6228cb2008661ac039723
admin <wojiaolinmu008@gmail.com>
2017-04-29 23:29:49 +0800

    Change status<shadowsocks>

--- shadowsocks/shadowsocks/scripts/ss_status.sh
+++ shadowsocks/shadowsocks/scripts/ss_status.sh
@@ -11,18 +11,18 @@ LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
 get_china_status(){
 	wget -4 --spider --quiet --tries=2 --timeout=2 www.baidu.com
 	if [ "$?" == "0" ]; then
-		log2='国内链接 【'$LOGTIME'】 working...'
+		log2='国内链接 【'$LOGTIME'】 ✓'
 	else
-		log2='国内链接 【'$LOGTIME'】 Problem detected!'
+		log2='国内链接 【'$LOGTIME'】 <font color='#FF0000'>X</font>'
 	fi
 }
 
 get_foreign_status(){
 	wget -4 --spider --quiet --tries=2 --timeout=2 www.google.com.tw
 	if [ "$?" == "0" ]; then
-		log1='国外链接 【'$LOGTIME'】 working...'
+		log1='国外链接 【'$LOGTIME'】 ✓'
 	else
-		log1='国外链接 【'$LOGTIME'】 Problem detected!'
+		log1='国外链接 【'$LOGTIME'】 <font color='#FF0000'>X</font>'
 	fi
 }
 
