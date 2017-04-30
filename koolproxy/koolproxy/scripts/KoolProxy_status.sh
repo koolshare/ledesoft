71a1d92520afe3970b56e34c4156b20c07fbc2ef
admin <wojiaolinmu008@gmail.com>
2017-04-30 22:35:51 +0800

    Change KoolProxy_status

--- koolproxy/koolproxy/scripts/KoolProxy_status.sh
+++ koolproxy/koolproxy/scripts/KoolProxy_status.sh
@@ -13,10 +13,11 @@ rules_date_local=`cat $KSROOT/koolproxy/data/rules/koolproxy.txt  | sed -n '3p'|
 rules_nu_local=`grep -E -v "^!" $KSROOT/koolproxy/data/rules/koolproxy.txt | wc -l`
 video_date_local=`cat $KSROOT/koolproxy/data/rules/koolproxy.txt  | sed -n '4p'|awk '{print $3,$4}'`
 daily_nu_local=`grep -E -v "^!" $KSROOT/koolproxy/data/rules/daily.txt | wc -l`
+custom_nu_local=`grep -E -v "^!" $KSROOT/koolproxy/data/rules/user.txt | wc -l`
 
 if [ "$status" == "2" ];then
-	http_response "【$date】 KoolProxy $version  进程运行正常！@@静态规则：$rules_date_local / $rules_nu_local条&nbsp;&nbsp;&nbsp;&nbsp;视频规则：$video_date_local&nbsp;&nbsp;&nbsp;&nbsp;每日规则：$daily_nu_local条"
+	http_response "【$date】 KoolProxy $version  进程运行正常！@@静态规则：$rules_date_local / $rules_nu_local条&nbsp;&nbsp;&nbsp;&nbsp;视频规则：$video_date_local&nbsp;&nbsp;&nbsp;&nbsp;每日/自定义规则：$daily_nu_local条 / $custom_nu_local条"
 else
-	http_response "【警告】：进程未运行！"
+	http_response "<font color='#FF0000'>【警告】：进程未运行！</font> @@<font color='#FF0000'>未加载！</font>"
 fi
 
