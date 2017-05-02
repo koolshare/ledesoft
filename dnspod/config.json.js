13bd9c15806994278009d2b77f2220792c9d1139
lixin <sadoneli@gmail.com>
2017-05-02 22:50:17 +0800

    fix title

--- dnspod/config.json.js
+++ dnspod/config.json.js
@@ -3,6 +3,6 @@
 "md5":"225f758ad9242079a6158b5111f14696",
 "home_url":"Module_dnspod.asp",
 "title":"dnspod ddns",
-"description":"",
+"description":"DNSPOD动态域名解析",
 "build_date":"2017-05-02_17:39:10"
 }
--- kuainiao/config.json.js
+++ kuainiao/config.json.js
@@ -2,7 +2,7 @@
 "version":"1.0.0",
 "md5":"5a2c0627e7d55fa1a59fe1b31245c0be",
 "home_url":"Module_kuainiao.asp",
-"title":"提升上下行带宽",
-"description":"",
+"title":"迅雷快鸟",
+"description":"上网带宽加速服务",
 "build_date":"2017-05-02_17:38:49"
 }
