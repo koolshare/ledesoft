9d09c1de67b26a558e053a853302c61b6705ae50
Kenney <zql254@vip.qq.com>
2017-05-03 13:54:37 +0800

    update config.sh

--- kuainiao/config.json.js
+++ kuainiao/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"1.0.2",
-"md5":"e72bc8b642d9934e0066f304e0367219",
+"md5":"a1fe5fc97cdf70f511fca0ade2bb8039",
 "home_url":"Module_kuainiao.asp",
 "title":"提升上下行带宽",
 "description":"",
-"build_date":"2017-05-03_13:19:50"
+"build_date":"2017-05-03_13:54:13"
 }
