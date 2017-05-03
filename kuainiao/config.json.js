76883c6e663f0a2e568f042e4d689c0f23d04def
Kenney <zql254@vip.qq.com>
2017-05-03 13:20:16 +0800

    fix keepalive

--- kuainiao/config.json.js
+++ kuainiao/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.0.1",
-"md5":"e91c1be6a1c8aaeecf56632a82f98d09",
+"version":"1.0.2",
+"md5":"e72bc8b642d9934e0066f304e0367219",
 "home_url":"Module_kuainiao.asp",
 "title":"提升上下行带宽",
 "description":"",
-"build_date":"2017-05-03_09:32:43"
+"build_date":"2017-05-03_13:19:50"
 }
