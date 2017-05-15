190df6b715557507f486d96decb1c0c6eb32a42a
houzi- <wojiaolinmu008@gmail.com>
2017-05-15 16:58:57 +0800

    Update version<xiaomi-fan-control>

--- xiaomi/config.json.js
+++ xiaomi/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.0.6",
-"md5":"cbf46a70d5fa305998b6ee375430fa2b",
+"version":"1.0.7",
+"md5":"2d213a39e8534a7969d3e65dbdd693f8",
 "home_url":"Module_xiaomi.asp",
 "title":"xiaomi fan control",
 "description":"小米R1D风扇控制插件",
-"build_date":"2017-05-14_23:36:15"
+"build_date":"2017-05-15_16:58:26"
 }
