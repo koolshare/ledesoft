6cbfdb920e8b52ea9e0658831989dfb821dbb658
lixin <sadoneli@gmail.com>
2017-05-17 20:56:04 +0800

    push message#2

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,6 +1,6 @@
 {
-"title":"<b>Tomato&nbsp;-&nbsp;软件中心</b>",
-"content1":"欢迎",
+"title":"<b>欢迎来到koolshare tomato软件中心</b>",
+"content1":"<b>通知：<b>",
 "content2":"如果你刷了v3.4-140-X1.3版本固件,而在【USB & NAS】标签页下没有【USB支持】的话，请按以下操作强制更新软件中心：",
-"content3":"1：安装并进入webshell插件&nbsp;&nbsp;&nbsp;&nbsp;2:输入命令 dbus set softcenter_version=0.3.0&nbsp;&nbsp;&nbsp;&nbsp;3,进入软件中心，更新软件中心。"
+"content3":"1：安装并进入webshell插件&nbsp;&nbsp;&nbsp;&nbsp;2：输入命令 dbus set softcenter_version=0.3.0&nbsp;&nbsp;&nbsp;&nbsp;3：进入软件中心，更新软件中心。"
 }
