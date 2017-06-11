4fff3078c5cefbfd42e4017b17421cea18c26c81
lixin <sadoneli@gmail.com>
2017-06-11 14:03:23 +0800

    fix ss udp

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2017-05-02_23:31:37", 
     "description": "fuck gfw", 
     "home_url": "Module_shadowsocks.asp", 
-    "md5": "41bf32be07f03f9aff2cbbc864a6ca03", 
+    "md5": "d00cdb2c8b49d887c6a12f41b493d5f0", 
     "module": "shadowsocks", 
     "title": "shadowsocks", 
     "version": "1.2.3"
