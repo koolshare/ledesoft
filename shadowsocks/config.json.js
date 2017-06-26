a55cb112b28d0998804d3b63857eeb2b5eab7538
lixin <sadoneli@gmail.com>
2017-06-26 20:53:57 +0800

    update

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2017-05-02_23:31:37", 
     "description": "fuck gfw", 
     "home_url": "Module_shadowsocks.asp", 
-    "md5": "4fcb9612eddc6cef67544d3fa1550798", 
+    "md5": "f4fbd5f246d5027cc200a97125b1a694", 
     "module": "shadowsocks", 
     "title": "shadowsocks", 
-    "version": "1.2.5"
+    "version": "1.2.6"
 }
