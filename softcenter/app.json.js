b94e0f3582008199b2f94650174076635d1b0970
lixin <sadoneli@gmail.com>
2017-07-10 17:36:11 +0800

    use smaller logo

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,54 +1,14 @@
 {
     "apps": [
         {
-            "build_date": "2017-07-09_16:21:33", 
-            "description": "Godaddy-DDNS", 
-            "home_url": "Module_gdddns.asp", 
-            "md5": "0d98ffc2fd200683d2d6971676635965", 
-            "name": "gdddns", 
-            "tar_url": "gdddns/gdddns.tar.gz", 
-            "title": "gdddns", 
-            "version": "1.0"
-        }, 
-        {
-            "build_date": "2017-07-10_11:24:04", 
-            "description": "VPN全家桶", 
-            "home_url": "Module_softether_vpn.asp", 
-            "md5": "edc8856bfd1436a9e385f00de2ab2133", 
-            "name": "softether_vpn", 
-            "tar_url": "softether_vpn/softether_vpn.tar.gz", 
-            "title": "softether_vpn", 
-            "version": "1.0.1"
-        }, 
-        {
-            "build_date": "2017-07-09_16:23:12", 
-            "description": "去头屑丝般顺滑", 
-            "home_url": "Module_koolproxy.asp", 
-            "md5": "5d2b127c20e485e4575b76496699953a", 
-            "name": "koolproxy", 
-            "tar_url": "koolproxy/koolproxy.tar.gz", 
-            "title": "koolproxy", 
-            "version": "3.6.1.0"
-        }, 
-        {
-            "build_date": "2017-07-10_11:24:19", 
-            "description": "巨硬套餐激活工具", 
-            "home_url": "Module_kms.asp", 
-            "md5": "3ce6690edeff32b9227746d4958945e6", 
-            "name": "kms", 
-            "tar_url": "kms/kms.tar.gz", 
-            "title": "kms", 
-            "version": "0.2"
-        }, 
-        {
-            "build_date": "2017-07-09_15:34:27", 
-            "description": "超酷的远程下载工具", 
-            "home_url": "Module_aria2.asp", 
-            "md5": "2f1d43acfd880ce8d5eebc50b9dc9d6c", 
-            "name": "aria2", 
-            "tar_url": "aria2/aria2.tar.gz", 
-            "title": "aria2", 
-            "version": "1.0"
+            "build_date": "2017-07-09_15:34:01", 
+            "description": "阿里云解析自动更新IP", 
+            "home_url": "Module_aliddns.asp", 
+            "md5": "08d0877fad8f84f364e38ad36f04f418", 
+            "name": "aliddns", 
+            "tar_url": "aliddns/aliddns.tar.gz", 
+            "title": "AliDDNS", 
+            "version": "0.1"
         }, 
         {
             "build_date": "2017-07-09_15:39:22", 
@@ -61,6 +21,86 @@
             "version": "1.0"
         }, 
         {
+            "build_date": "2017-07-10_11:24:19", 
+            "description": "巨硬套餐激活工具", 
+            "home_url": "Module_kms.asp", 
+            "md5": "3ce6690edeff32b9227746d4958945e6", 
+            "name": "kms", 
+            "tar_url": "kms/kms.tar.gz", 
+            "title": "kms", 
+            "version": "0.2"
+        }, 
+        {
+            "build_date": "2017-07-09_16:21:33", 
+            "description": "Godaddy-DDNS", 
+            "home_url": "Module_gdddns.asp", 
+            "md5": "0d98ffc2fd200683d2d6971676635965", 
+            "name": "gdddns", 
+            "tar_url": "gdddns/gdddns.tar.gz", 
+            "title": "gdddns", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_15:34:27", 
+            "description": "超酷的远程下载工具", 
+            "home_url": "Module_aria2.asp", 
+            "md5": "2f1d43acfd880ce8d5eebc50b9dc9d6c", 
+            "name": "aria2", 
+            "tar_url": "aria2/aria2.tar.gz", 
+            "title": "aria2", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-10_11:24:33", 
+            "description": "一个轻量级SOCKS5代理", 
+            "home_url": "Module_shadowsocks.asp", 
+            "md5": "897875b0348329187839cdac6b14dd6b", 
+            "name": "shadowsocks", 
+            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
+            "title": "shadowsocks", 
+            "version": "1.1"
+        }, 
+        {
+            "build_date": "2017-07-10_11:24:04", 
+            "description": "VPN全家桶", 
+            "home_url": "Module_softether_vpn.asp", 
+            "md5": "edc8856bfd1436a9e385f00de2ab2133", 
+            "name": "softether_vpn", 
+            "tar_url": "softether_vpn/softether_vpn.tar.gz", 
+            "title": "softether_vpn", 
+            "version": "1.0.1"
+        }, 
+        {
+            "build_date": "2017-07-09_15:40:23", 
+            "description": "多终端同步工具", 
+            "home_url": "Module_syncthing.asp", 
+            "md5": "e7c820d0f2d25d00cb43b32157892f86", 
+            "name": "syncthing", 
+            "tar_url": "syncthing/syncthing.tar.gz", 
+            "title": "syncthing", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_15:36:45", 
+            "description": "上下行带宽提速", 
+            "home_url": "Module_kuainiao.asp", 
+            "md5": "a9e3900000e1e431706da7249475b096", 
+            "name": "kuainiao", 
+            "tar_url": "kuainiao/kuainiao.tar.gz", 
+            "title": "kuainiao", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-10_11:24:54", 
+            "description": "支持http2协议的快速穿透", 
+            "home_url": "Module_ddnsto.asp", 
+            "md5": "2aa32d7722caf8c6ea1445b0586770bc", 
+            "name": "ddnsto", 
+            "tar_url": "ddnsto/ddnsto.tar.gz", 
+            "title": "ddnsto", 
+            "version": "1.1"
+        }, 
+        {
             "build_date": "2017-07-09_15:34:59", 
             "description": "DNSPOD动态域名解析", 
             "home_url": "Module_dnspod.asp", 
@@ -69,6 +109,16 @@
             "tar_url": "dnspod/dnspod.tar.gz", 
             "title": "dnspod", 
             "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_16:23:12", 
+            "description": "去头屑丝般顺滑", 
+            "home_url": "Module_koolproxy.asp", 
+            "md5": "5d2b127c20e485e4575b76496699953a", 
+            "name": "koolproxy", 
+            "tar_url": "koolproxy/koolproxy.tar.gz", 
+            "title": "koolproxy", 
+            "version": "3.6.1.0"
         }, 
         {
             "build_date": "2017-07-09_15:35:34", 
@@ -89,60 +139,10 @@
             "tar_url": "shellinabox/shellinabox.tar.gz", 
             "title": "ShellnaBox", 
             "version": "1.1"
-        }, 
-        {
-            "build_date": "2017-07-09_15:36:45", 
-            "description": "上下行带宽提速", 
-            "home_url": "Module_kuainiao.asp", 
-            "md5": "a9e3900000e1e431706da7249475b096", 
-            "name": "kuainiao", 
-            "tar_url": "kuainiao/kuainiao.tar.gz", 
-            "title": "kuainiao", 
-            "version": "1.0"
-        }, 
-        {
-            "build_date": "2017-07-09_15:34:01", 
-            "description": "阿里云解析自动更新IP", 
-            "home_url": "Module_aliddns.asp", 
-            "md5": "08d0877fad8f84f364e38ad36f04f418", 
-            "name": "aliddns", 
-            "tar_url": "aliddns/aliddns.tar.gz", 
-            "title": "AliDDNS", 
-            "version": "0.1"
-        }, 
-        {
-            "build_date": "2017-07-09_15:40:23", 
-            "description": "多终端同步工具", 
-            "home_url": "Module_syncthing.asp", 
-            "md5": "e7c820d0f2d25d00cb43b32157892f86", 
-            "name": "syncthing", 
-            "tar_url": "syncthing/syncthing.tar.gz", 
-            "title": "syncthing", 
-            "version": "1.0"
-        }, 
-        {
-            "build_date": "2017-07-10_11:24:33", 
-            "description": "一个轻量级SOCKS5代理", 
-            "home_url": "Module_shadowsocks.asp", 
-            "md5": "897875b0348329187839cdac6b14dd6b", 
-            "name": "shadowsocks", 
-            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
-            "title": "shadowsocks", 
-            "version": "1.1"
-        }, 
-        {
-            "build_date": "2017-07-10_11:24:54", 
-            "description": "支持http2协议的快速穿透", 
-            "home_url": "Module_ddnsto.asp", 
-            "md5": "2aa32d7722caf8c6ea1445b0586770bc", 
-            "name": "ddnsto", 
-            "tar_url": "ddnsto/ddnsto.tar.gz", 
-            "title": "ddnsto", 
-            "version": "1.1"
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
-    "md5": "7a30b0ca4f7abdc76aa73e00cfa8e5b9", 
+    "md5": "b15beecab39fc9ad84237792cf7a6a07", 
     "tar_url": "softcenter/softcenter.tar.gz", 
-    "version": "0.1.5"
+    "version": "0.1.6"
 }
--- softcenter/config.json.js
+++ softcenter/config.json.js
@@ -1,4 +1,4 @@
 {
-"version":"0.1.5",
-"md5":"7a30b0ca4f7abdc76aa73e00cfa8e5b9"
+"version":"0.1.6",
+"md5":"b15beecab39fc9ad84237792cf7a6a07"
 }
