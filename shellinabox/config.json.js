5992744c1a5ad63a0c364f3cce9c580aabe0cf4e
fw867 <fw867@163.com>
2017-07-10 17:06:15 +0800

    shellinabox run whithout https

--- shellinabox/config.json.js
+++ shellinabox/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"1.1",
-"md5":"dcdcaa3c523d6cd27c99f8129359d719",
+"md5":"cd8f3331a903931fba1baf20c8c39f4a",
 "home_url":"Module_shellinabox.asp",
 "title":"ShellnaBox",
 "description":"超强的SSH网页客户端",
-"build_date":"2017-07-10_11:25:46"
+"build_date":"2017-07-10_17:04:32"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,24 +1,34 @@
 {
     "apps": [
         {
-            "build_date": "2017-07-09_15:34:01", 
-            "description": "阿里云解析自动更新IP", 
-            "home_url": "Module_aliddns.asp", 
-            "md5": "08d0877fad8f84f364e38ad36f04f418", 
-            "name": "aliddns", 
-            "tar_url": "aliddns/aliddns.tar.gz", 
-            "title": "AliDDNS", 
-            "version": "0.1"
+            "build_date": "2017-07-09_16:21:33", 
+            "description": "Godaddy-DDNS", 
+            "home_url": "Module_gdddns.asp", 
+            "md5": "0d98ffc2fd200683d2d6971676635965", 
+            "name": "gdddns", 
+            "tar_url": "gdddns/gdddns.tar.gz", 
+            "title": "gdddns", 
+            "version": "1.0"
         }, 
         {
-            "build_date": "2017-07-09_15:39:22", 
-            "description": "虚拟内存~", 
-            "home_url": "Module_swap.asp", 
-            "md5": "9cf24265f8f70803c086c955b945c3b5", 
-            "name": "swap", 
-            "tar_url": "swap/swap.tar.gz", 
-            "title": "swap", 
-            "version": "1.0"
+            "build_date": "2017-07-10_11:24:04", 
+            "description": "VPN全家桶", 
+            "home_url": "Module_softether_vpn.asp", 
+            "md5": "edc8856bfd1436a9e385f00de2ab2133", 
+            "name": "softether_vpn", 
+            "tar_url": "softether_vpn/softether_vpn.tar.gz", 
+            "title": "softether_vpn", 
+            "version": "1.0.1"
+        }, 
+        {
+            "build_date": "2017-07-09_16:23:12", 
+            "description": "去头屑丝般顺滑", 
+            "home_url": "Module_koolproxy.asp", 
+            "md5": "5d2b127c20e485e4575b76496699953a", 
+            "name": "koolproxy", 
+            "tar_url": "koolproxy/koolproxy.tar.gz", 
+            "title": "koolproxy", 
+            "version": "3.6.1.0"
         }, 
         {
             "build_date": "2017-07-10_11:24:19", 
@@ -31,16 +41,6 @@
             "version": "0.2"
         }, 
         {
-            "build_date": "2017-07-09_16:21:33", 
-            "description": "Godaddy-DDNS", 
-            "home_url": "Module_gdddns.asp", 
-            "md5": "0d98ffc2fd200683d2d6971676635965", 
-            "name": "gdddns", 
-            "tar_url": "gdddns/gdddns.tar.gz", 
-            "title": "gdddns", 
-            "version": "1.0"
-        }, 
-        {
             "build_date": "2017-07-09_15:34:27", 
             "description": "超酷的远程下载工具", 
             "home_url": "Module_aria2.asp", 
@@ -48,6 +48,76 @@
             "name": "aria2", 
             "tar_url": "aria2/aria2.tar.gz", 
             "title": "aria2", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_15:39:22", 
+            "description": "虚拟内存~", 
+            "home_url": "Module_swap.asp", 
+            "md5": "9cf24265f8f70803c086c955b945c3b5", 
+            "name": "swap", 
+            "tar_url": "swap/swap.tar.gz", 
+            "title": "swap", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_15:34:59", 
+            "description": "DNSPOD动态域名解析", 
+            "home_url": "Module_dnspod.asp", 
+            "md5": "e391cee93de8c1fa63c9c2b34ee19b12", 
+            "name": "dnspod", 
+            "tar_url": "dnspod/dnspod.tar.gz", 
+            "title": "dnspod", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_15:35:34", 
+            "description": "FRPC内网穿透工具", 
+            "home_url": "Module_frpc.asp", 
+            "md5": "e173254e3630b0ea42bf70fb811c1a21", 
+            "name": "frpc", 
+            "tar_url": "frpc/frpc.tar.gz", 
+            "title": "frpc", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-10_17:04:32", 
+            "description": "超强的SSH网页客户端", 
+            "home_url": "Module_shellinabox.asp", 
+            "md5": "cd8f3331a903931fba1baf20c8c39f4a", 
+            "name": "shellinabox", 
+            "tar_url": "shellinabox/shellinabox.tar.gz", 
+            "title": "ShellnaBox", 
+            "version": "1.1"
+        }, 
+        {
+            "build_date": "2017-07-09_15:36:45", 
+            "description": "上下行带宽提速", 
+            "home_url": "Module_kuainiao.asp", 
+            "md5": "a9e3900000e1e431706da7249475b096", 
+            "name": "kuainiao", 
+            "tar_url": "kuainiao/kuainiao.tar.gz", 
+            "title": "kuainiao", 
+            "version": "1.0"
+        }, 
+        {
+            "build_date": "2017-07-09_15:34:01", 
+            "description": "阿里云解析自动更新IP", 
+            "home_url": "Module_aliddns.asp", 
+            "md5": "08d0877fad8f84f364e38ad36f04f418", 
+            "name": "aliddns", 
+            "tar_url": "aliddns/aliddns.tar.gz", 
+            "title": "AliDDNS", 
+            "version": "0.1"
+        }, 
+        {
+            "build_date": "2017-07-09_15:40:23", 
+            "description": "多终端同步工具", 
+            "home_url": "Module_syncthing.asp", 
+            "md5": "e7c820d0f2d25d00cb43b32157892f86", 
+            "name": "syncthing", 
+            "tar_url": "syncthing/syncthing.tar.gz", 
+            "title": "syncthing", 
             "version": "1.0"
         }, 
         {
@@ -61,36 +131,6 @@
             "version": "1.1"
         }, 
         {
-            "build_date": "2017-07-10_11:24:04", 
-            "description": "VPN全家桶", 
-            "home_url": "Module_softether_vpn.asp", 
-            "md5": "edc8856bfd1436a9e385f00de2ab2133", 
-            "name": "softether_vpn", 
-            "tar_url": "softether_vpn/softether_vpn.tar.gz", 
-            "title": "softether_vpn", 
-            "version": "1.0.1"
-        }, 
-        {
-            "build_date": "2017-07-09_15:40:23", 
-            "description": "多终端同步工具", 
-            "home_url": "Module_syncthing.asp", 
-            "md5": "e7c820d0f2d25d00cb43b32157892f86", 
-            "name": "syncthing", 
-            "tar_url": "syncthing/syncthing.tar.gz", 
-            "title": "syncthing", 
-            "version": "1.0"
-        }, 
-        {
-            "build_date": "2017-07-09_15:36:45", 
-            "description": "上下行带宽提速", 
-            "home_url": "Module_kuainiao.asp", 
-            "md5": "a9e3900000e1e431706da7249475b096", 
-            "name": "kuainiao", 
-            "tar_url": "kuainiao/kuainiao.tar.gz", 
-            "title": "kuainiao", 
-            "version": "1.0"
-        }, 
-        {
             "build_date": "2017-07-10_11:24:54", 
             "description": "支持http2协议的快速穿透", 
             "home_url": "Module_ddnsto.asp", 
@@ -98,46 +138,6 @@
             "name": "ddnsto", 
             "tar_url": "ddnsto/ddnsto.tar.gz", 
             "title": "ddnsto", 
-            "version": "1.1"
-        }, 
-        {
-            "build_date": "2017-07-09_15:34:59", 
-            "description": "DNSPOD动态域名解析", 
-            "home_url": "Module_dnspod.asp", 
-            "md5": "e391cee93de8c1fa63c9c2b34ee19b12", 
-            "name": "dnspod", 
-            "tar_url": "dnspod/dnspod.tar.gz", 
-            "title": "dnspod", 
-            "version": "1.0"
-        }, 
-        {
-            "build_date": "2017-07-09_16:23:12", 
-            "description": "去头屑丝般顺滑", 
-            "home_url": "Module_koolproxy.asp", 
-            "md5": "5d2b127c20e485e4575b76496699953a", 
-            "name": "koolproxy", 
-            "tar_url": "koolproxy/koolproxy.tar.gz", 
-            "title": "koolproxy", 
-            "version": "3.6.1.0"
-        }, 
-        {
-            "build_date": "2017-07-09_15:35:34", 
-            "description": "FRPC内网穿透工具", 
-            "home_url": "Module_frpc.asp", 
-            "md5": "e173254e3630b0ea42bf70fb811c1a21", 
-            "name": "frpc", 
-            "tar_url": "frpc/frpc.tar.gz", 
-            "title": "frpc", 
-            "version": "1.0"
-        }, 
-        {
-            "build_date": "2017-07-10_11:25:46", 
-            "description": "超强的SSH网页客户端", 
-            "home_url": "Module_shellinabox.asp", 
-            "md5": "dcdcaa3c523d6cd27c99f8129359d719", 
-            "name": "shellinabox", 
-            "tar_url": "shellinabox/shellinabox.tar.gz", 
-            "title": "ShellnaBox", 
             "version": "1.1"
         }
     ], 
