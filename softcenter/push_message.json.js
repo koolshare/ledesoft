c0194061b69d54cef8e68dfda43de1a0bff27711
sadoneli <sadoneli@gmail.com>
2017-08-08 00:46:38 +0800

    update message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,5 +1,5 @@
 {
 "title":"<b>欢迎来到koolshare lede软件中心</b>",
-"content1":"<font color='#FF0033'><b>通知：<b>LEDE X64 V2.0已发，更新日志和下载地址请见：<a href='http://koolshare.cn/thread-64061-1-1.html'>http://koolshare.cn/thread-64061-1-1.html</a></font>",
-"content2":"<font color='#1E90FF'>BUG反馈:<a href='https://t.me/joinchat/ERO9vEMMVu1dzQ-F8nP6kA'>1.加入TG群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://shang.qq.com/wpa/qunwpa?idkey=9689c491e45fdb982da6dd5dc7bb7c8dbeba427e358e289103b26fd43ca77ad0'>2.加入QQ群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://koolshare.cn/forum-97-1.html'>3.Koolshare论坛LEDE版块</a></font>",
+"content1":"<font color='#FF0033'><b>通知：<b>LEDE X64 V2.0已发，更新日志和下载地址请见：<a href='http://koolshare.cn/thread-64061-1-1.html'  target='_blank'>http://koolshare.cn/thread-64061-1-1.html</a></font>",
+"content2":"<font color='#1E90FF'>BUG反馈:&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://t.me/joinchat/ERO9vEMMVu1dzQ-F8nP6kA' target='_blank'>1.加入TG群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://shang.qq.com/wpa/qunwpa?idkey=9689c491e45fdb982da6dd5dc7bb7c8dbeba427e358e289103b26fd43ca77ad0' target='_blank'>2.加入QQ群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://koolshare.cn/forum-97-1.html' target='_blank'>3.Koolshare论坛LEDE版块</a></font>",
 }
