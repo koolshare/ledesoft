655c959f5ce7a1dfbf449b6602ebffc07778732a
sadoneli <sadoneli@gmail.com>
2017-08-10 01:56:15 +0800

    update kp logo

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"3.6.1.16",
-"md5":"922d59907fffb4f20157b64aa99360b8",
+"md5":"56c9b0e2c834d1f0d014ae9d1435a216",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"听说KP和软路由更搭哦~",
-"build_date":"2017-08-10_01:34:17"
+"build_date":"2017-08-10_01:55:59"
 }
