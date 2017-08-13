5ebfbc65b657d00c9ceb8debefcd3094a463f1eb
fw867 <fw867@163.com>
2017-08-13 19:02:28 +0800

    add ftp

---
+++ ftp/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.0",
+"md5":"7996ba7ef32ca0c57581af4c939dada0",
+"home_url":"Module_ftp.asp",
+"title":"FTP服务器",
+"description":"小巧安全的FTP服务器",
+"build_date":"2017-08-13_19:02:12"
+}
---
+++ ftp/ftp/scripts/ftp_config.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+export KSROOT=/koolshare
+source $KSROOT/scripts/base.sh
+eval `dbus export ftp_`
+
+config_anonymous(){
+	if [ "$ftp_anonymous" == "1" ]; then
+		sed -i 's/anonymous_enable=NO/anonymous_enable=YES/g' $KSROOT/ftp/ftp.conf
+	else
+		sed -i 's/anonymous_enable=YES/anonymous_enable=NO/g' $KSROOT/ftp/ftp.conf
+	fi
+}
+
+start_ftp(){
+	mkdir -p /tmp/anymous
+	chown root:nogroup /tmp/anymous
+	chmod 557 /tmp/anymous
+	$KSROOT/ftp/ftp $KSROOT/ftp/ftp.conf
+}
+
+stop_ftp(){
+	killall -q -9 ftp
+}
+
+creat_start_up(){
+	[ ! -L "/etc/rc.d/S94ftp.sh" ] && ln -sf /koolshare/init.d/S94ftp.sh /etc/rc.d/S94ftp.sh
+}
+
+del_start_up(){
+	rm -rf /etc/rc.d/S94ftp.sh >/dev/null 2>&1
+}
+
+
+if [ "$ftp_enable" == "1" ]; then
+	del_start_up
+	stop_ftp
+	sleep 1
+	config_anonymous
+	start_ftp
+	creat_start_up
+   	http_response '服务已开启！页面将在3秒后刷新'
+else
+	stop_ftp
+	del_start_up
+	http_response '服务已关闭！页面将在3秒后刷新'
+fi
+
