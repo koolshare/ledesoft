becaa7047246020b8f29f1c0012cbfd4b28bba5b
fw867 <fw867@163.com>
2017-08-18 18:56:25 +0800

    fix aria2 dirpath

--- aria2/build.sh
+++ aria2/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MODULE=aria2
-VERSION=1.3
+VERSION=1.4
 TITLE=aria2
 DESCRIPTION=超酷的远程下载工具
 HOME_URL=Module_aria2.asp
--- aria2/config.json.js
+++ aria2/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.3",
-"md5":"f87972f6c280e1d2728524c2e65a5829",
+"version":"1.4",
+"md5":"ec2b335622b2430336bd447c4e274f69",
 "home_url":"Module_aria2.asp",
 "title":"aria2",
 "description":"超酷的远程下载工具",
-"build_date":"2017-08-09_23:24:33"
+"build_date":"2017-08-18_18:55:52"
 }
