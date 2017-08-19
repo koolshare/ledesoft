c75161d387aca7ff1f4689491e5a4591be1b5c12
sadoneli <sadoneli@gmail.com>
2017-08-19 21:18:17 +0800

    update koolproxy

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.6.1.27",
-"md5":"baaf851b0c15ee18da06709352f4a16b",
+"version":"3.6.1.28",
+"md5":"ea173893e6b01111748f8f8e6bac5c5c",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"听说KP和软路由更搭哦~",
-"build_date":"2017-08-18_17:52:11"
+"build_date":"2017-08-19_21:18:06"
 }
