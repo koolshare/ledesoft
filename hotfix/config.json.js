43670d0f3c6e2c9de7629f464002c47e9abcf7f3
sadoneli <sadoneli@gmail.com>
2017-08-25 15:55:21 +0800

    fix hotfix

--- hotfix/config.json.js
+++ hotfix/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"0.1.2",
-"md5":"78166cabc4856ca062942623ae24c09b",
+"version":"0.1.3",
+"md5":"2b6e8e0f77abc9482c527edd7f486cb3",
 "home_url":"Module_hotfix.asp",
 "title":"HOTFIX",
 "description":"快速修复当前固件中的BUG",
-"build_date":"2017-08-25_15:49:14"
+"build_date":"2017-08-25_15:55:12"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -161,14 +161,14 @@
             "version": "0.6"
         }, 
         {
-            "build_date": "2017-08-25_15:49:14", 
+            "build_date": "2017-08-25_15:55:12", 
             "description": "快速修复当前固件中的BUG", 
             "home_url": "Module_hotfix.asp", 
-            "md5": "78166cabc4856ca062942623ae24c09b", 
+            "md5": "2b6e8e0f77abc9482c527edd7f486cb3", 
             "name": "hotfix", 
             "tar_url": "hotfix/hotfix.tar.gz", 
             "title": "HOTFIX", 
-            "version": "0.1.2"
+            "version": "0.1.3"
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
