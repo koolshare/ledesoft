f9937fe554d82008d263c98005b5f90b1caedd36
sadoneli <sadoneli@gmail.com>
2017-09-12 10:18:18 +0800

    update

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,10 +1,12 @@
+[
 {
     "build_date": "2017-09-09_18:29:47", 
     "description": "轻松科学上网~", 
     "home_url": "Module_shadowsocks.asp", 
-    "md5": "6888c11e6045ac6aded74e54c11539cd", 
+    "md5": "40ba9020111d0c79953b5b8310502e57", 
     "name": "shadowsocks", 
     "tar_url": "shadowsocks/shadowsocks.tar.gz", 
     "title": "shadowsocks", 
     "version": "1.7.1"
 }
+]
