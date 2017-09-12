5e19af6a23d38bb1692fda5d57bf2a130e5ba11a
fw867 <fw867@163.com>
2017-09-13 01:09:36 +0800

    add homebridge

---
+++ homebridge/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"0.1",
+"md5":"f4a250ab43cb6101ebb8953c9b041738",
+"home_url":"Module_homebridge.asp",
+"title":"Homebridge",
+"description":"智能家庭网关",
+"build_date":"2017-09-13_01:08:14"
+}
