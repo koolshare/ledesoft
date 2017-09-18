959ea62d50243d34d3554226040fa6fc3da3b924
fw867 <fw867@163.com>
2017-09-19 00:52:45 +0800

    update message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,6 +1,6 @@
 {
 "title":"<b>欢迎来到koolshare lede软件中心</b>",
-"content1":"<font color='#FF0033'><b>通知：<b>LEDE X64 V2.4已发，更新日志和下载地址请见：<a href='http://koolshare.cn/thread-64061-1-1.html'  target='_blank'>http://koolshare.cn/thread-64061-1-1.html</a></font>",
+"content1":"<font color='#FF0033'><b>通知：<b>LEDE X64 V2.5已发，更新日志和下载地址请见：<a href='http://koolshare.cn/thread-120516-1-1.html'  target='_blank'>http://koolshare.cn/thread-120516-1-1.html</a></font>",
 "content2":"<font color='#1E90FF'>软件中心0.2.2更新日志:&nbsp;&nbsp; 更新软件中心二进制文件，增强稳定性。",
 "content3":"<font color='#1E90FF'>BUG反馈:&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://t.me/joinchat/ERO9vEMMVu1dzQ-F8nP6kA' target='_blank'>1.加入TG群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://shang.qq.com/wpa/qunwpa?idkey=9689c491e45fdb982da6dd5dc7bb7c8dbeba427e358e289103b26fd43ca77ad0' target='_blank'>2.加入QQ群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://koolshare.cn/forum-97-1.html' target='_blank'>3.Koolshare论坛LEDE版块</a></font>",
 }
