e1aed9082edd454e5dcb33968339abed88854489
fw867 <fw867@163.com>
2017-09-19 15:42:19 +0800

    update message

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,9 +1,9 @@
 {
-"title":"<font color='#FF0033'><b>Lede X64 V2.5 r3435 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Lede X64 V2.6 r3435 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
-<br/>LEDE软件中心已经发布，频繁的更新固件已经没有必要，所以这次基于LEDE17.01.2重建固件。<br/>",
+<br/>LEDE软件中心已经发布，所以基于LEDE17.01.2稳定版重建固件。<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级*s*到3.1.0；<br />\
-<b>2. </b>miniupnp增加多wan指定出口；<br />\
-<b>3. </b>修复版本号过低的bug。<br />",
+<b>1. </b>降级dnsmasq到2.76；<br />\
+<b>2. </b>修复主题输入框没有居中的问题；<br />\
+<b>3. </b>修复升级固件后插件无法自动运行的问题。<br />",
 }
