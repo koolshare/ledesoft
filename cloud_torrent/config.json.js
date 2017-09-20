b0c5da8cca8af9eb1ba4a797bcd4250a71809700
fw867 <fw867@163.com>
2017-09-20 13:44:49 +0800

    update cloud_torrent bg

--- cloud_torrent/config.json.js
+++ cloud_torrent/config.json.js
@@ -1,9 +1,9 @@
 {
 "version":"1.3",
-"md5":"72e0b9ead1a9481f42127db0dd0c1cc3",
+"md5":"6f555e9fc422be0d86779f51b4cff6b3",
 "home_url":"Module_cloud_torrent.asp",
 "title":"cloud torrent",
 "description":"小巧便携BT下载器",
 "changelog":"",
-"build_date":"2017-09-20_02:41:55"
+"build_date":"2017-09-20_13:44:05"
 }
