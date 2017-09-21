77f525298ce07b50aeef8edba054730a9fd08b85
fw867 <fw867@163.com>
2017-09-21 18:58:04 +0800

    add lnmp

---
+++ lnmp/config.json.js
@@ -0,0 +1,9 @@
+{
+"version":"0.1",
+"md5":"d2c1b306abdf03a2e56895e09c7fa35a",
+"home_url":"Module_lnmp.asp",
+"title":"LNMP",
+"description":"自动化部署WEB环境",
+"changelog":"",
+"build_date":"2017-09-21_18:52:59"
+}
