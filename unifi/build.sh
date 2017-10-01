f30d8ab70233f911ac99c8a7b4ec6ec44a13b9a8
fw867 <fw867@163.com>
2017-10-01 16:24:45 +0800

    add unifi controller

---
+++ unifi/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+MODULE=unifi
+VERSION=0.1
+TITLE="Unifi Controller"
+DESCRIPTION=Debian系统和Unifi控制器
+HOME_URL=Module_unifi.asp
+
+# Check and include base
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+
+# now include build_base.sh
+. $DIR/../softcenter/build_base.sh
+
+# change to module directory
+cd $DIR
+
+# build bin
+sh $DIR/build/build
+
+# do something here
+
+do_build_result
+
+sh backup.sh $MODULE
---
+++ unifi/config.json.js
@@ -0,0 +1,9 @@
+{
+"version":"0.1",
+"md5":"267b20ae2fb551610d281f0939f0dece",
+"home_url":"Module_unifi.asp",
+"title":"Unifi Controller",
+"description":"Debian系统和Unifi控制器",
+"changelog":"",
+"build_date":"2017-10-01_16:23:02"
+}
