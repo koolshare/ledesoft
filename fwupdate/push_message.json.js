a70bf4bc259797bd6a60aa9604cdbf41259c7a23
fw867 <fw867@163.com>
2017-10-09 22:34:23 +0800

    V2.7 firmware update log

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,9 +1,10 @@
 {
-"title":"<font color='#FF0033'><b>Lede X64 V2.6 r3435 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Lede X64 V2.7 r5620 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
-<br/>LEDE软件中心已经发布，所以基于LEDE17.01.2稳定版重建固件。<br/>",
+<br/>你们都是内核控。。。我投降<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>降级dnsmasq到2.76；<br />\
-<b>2. </b>修复主题输入框没有居中的问题；<br />\
-<b>3. </b>修复升级固件后插件无法自动运行的问题。<br />",
+<b>1. </b>升级dnsmasq到2.78；<br />\
+<b>2. </b>升级内核到4.9.54；<br />\
+<b>3. </b>增加上网认证；<br />\
+<b>4. </b>修复老平台cpu无法降频的问题。<br />",
 }
