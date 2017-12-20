9ccfadb17569d1db2b9be21ad163f2f3c01b51da
fw867 <fw867@163.com>
2017-12-20 11:53:18 +0800

    update message for 2.12

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,9 +1,8 @@
 {
-"title":"<font color='#FF0033'><b>Lede X64 V2.11 r6075 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Lede X64 V2.12 r6196 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>日常更新又来了！<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.9.67；<br />\
-<b>2. </b>增加一些集成内核模块；<br />\
-<b>3. </b>继续精简固件，从内核去掉了石像鬼QOS的支持，相关的QOS删除<br />",
+<b>1. </b>升级内核到4.9.70；<br />\
+<b>2. </b>主要是修复内核过低的bug...<br />",
 }
