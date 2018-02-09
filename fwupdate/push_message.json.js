c5c7667bae76042f1df2ee17220edf966446b3a5
fw867 <fw867@163.com>
2018-02-09 13:17:28 +0800

    update message for 2.15

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,8 +1,8 @@
 {
-"title":"<font color='#FF0033'><b>Lede X64 V2.14 r6384 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.15 r6665 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本会清除所有配置。<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.9.76；<br />\
-<b>2. </b>日常填旧坑，挖新坑...<br />",
+<b>1. </b>升级内核到4.9.77；<br />\
+<b>2. </b>重新加入石像鬼QOS...<br />",
 }
