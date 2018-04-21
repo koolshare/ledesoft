7d90c5b55e235436652cf817c6504096f7c8e64c
fw867 <fw867@163.com>
2018-04-21 19:08:40 +0800

    update message for 2.16

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,8 +1,9 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.15 r6665 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.16 r7387 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本会清除所有配置。<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.9.77；<br />\
-<b>2. </b>重新加入石像鬼QOS...<br />",
+<b>1. </b>升级内核到4.14.37(暂时不支持石像鬼QOS、SFE加速引擎)；<br />\
+<b>2. </b>修复恢复默认配置按钮未显示的问题<br />\
+<b>3. </b>UEFI引导支持<br />",
 }
