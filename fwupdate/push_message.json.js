3789e91cc70402bfc4da8ebcbaf590499fdd573e
fw867 <fw867@163.com>
2018-05-01 16:04:36 +0800

    update message for 2.17

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,9 +1,10 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.16 r7387 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.17 r7469 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本会清除所有配置。<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.14.34(暂时不支持石像鬼QOS、SFE加速引擎)；<br />\
-<b>2. </b>修复恢复默认配置按钮未显示的问题<br />\
-<b>3. </b>UEFI引导支持<br />",
+<b>1. </b>升级内核到4.14.37(暂时不支持石像鬼QOS、SFE加速引擎)；<br />\
+<b>2. </b>修复luci无法保存网络设置的问题<br />\
+<b>3. </b>修复一些链接失踪的问题<br />\
+<b>4. </b>增加内核NAT加速<br />",
 }
--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,6 +1,6 @@
 {
 "title":"<b>欢迎来到koolshare Openwrt软件中心</b>",
-"content1":"<font color='#FF0033'><b>通知：<b>Openwrt X64 V2.16已发，更新日志和下载地址请见：<a href='http://koolshare.cn/thread-120516-1-1.html'  target='_blank'>http://koolshare.cn/thread-120516-1-1.html</a></font>",
+"content1":"<font color='#FF0033'><b>通知：<b>Openwrt X64 V2.17已发，更新日志和下载地址请见：<a href='http://koolshare.cn/thread-120516-1-1.html'  target='_blank'>http://koolshare.cn/thread-120516-1-1.html</a></font>",
 "content2":"<font color='#1E90FF'>提示:&nbsp;&nbsp;建议使用Chrome浏览器打开LEDE软件中心。",
 "content3":"<font color='#CC00CC'>软件中心内插件会持续为最新版本固件优化升级，为了更好的体验，强烈建议使用最新固件！",
 "content4":"<font color='#1E90FF'>BUG反馈:&nbsp;&nbsp;<a href='https://t.me/joinchat/DCq55kMMVu3lngw1wY6Zng' target='_blank'>1.加入TG群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://shang.qq.com/wpa/qunwpa?idkey=9689c491e45fdb982da6dd5dc7bb7c8dbeba427e358e289103b26fd43ca77ad0' target='_blank'>2.加入QQ群</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='http://koolshare.cn/forum-97-1.html' target='_blank'>3.Koolshare论坛LEDE版块</a></font>",
