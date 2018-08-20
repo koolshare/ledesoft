2f5d7c2ae1575448e6842030188db6cfbe09930f
fw867 <fw867@163.com>
2018-08-20 13:41:16 +0800

    update entware

--- entware/config.json.js
+++ entware/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"0.1",
-"md5":"241ab95abf082074bc36f59d6a3587a9",
+"version":"0.2",
+"md5":"f9f662d41aa958e718fa13015ced0d93",
 "home_url":"Module_entware.asp",
 "title":"Entware",
 "description":"Entware环境和扩展",
-"changelog":"",
-"build_date":"2018-01-05_19:19:30"
+"changelog":"升级ONMP到2018-08-08",
+"build_date":"2018-08-20_13:40:47"
 }
