c95c96eb6af312f7c07754ef48b84cf14dd5b45a
fw867 <fw867@163.com>
2018-09-16 17:57:14 +0800

    update message for 2.20

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,10 +1,10 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.19 r8092 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.20 r8820 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.14.52；<br />\
-<b>2. </b>增加一些万兆网卡驱动<br />\
+<b>1. </b>升级内核到4.14.68；<br />\
+<b>2. </b>内核增加LXC、Docker支持<br />\
 <b>3. </b>修复一些已知BUG<br />\
-<b>4. </b>由于advanced tomato主题早已不再维护，为了避免以后可能出现的界面问题，移除了toamto主题，从2.19开始使用openwrt默认的bootstrap主题<br />",
+<b>4. </b>Advanced tomato主题回归了。保留配置升级的可以在主题设置里切换到ATT主题。<br />",
 }
