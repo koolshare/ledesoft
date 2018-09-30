584d365fa330596fe070e524eeb6a3d96b877f85
fw867 <fw867@163.com>
2018-09-30 14:21:14 +0800

    update for 2.23

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,10 +1,9 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.22 r8838 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.23 r8972 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>修复qemu代理导致某些物理主机重启的问题；<br />\
-<b>2. </b>修复访问限制无法使用的问题；<br />\
-<b>3. </b>修复NTFS、FAT格式硬盘的挂载问题；<br />\
-<b>4. </b>升级Samba到V4,支持Time-machine、Netbios。<br />",
+<b>1. </b>升级内核到4.14.72；<br />\
+<b>2. </b>默认开启full clone,无需安装插件；<br />\
+<b>3. </b>修复bugs；<br />",
 }
