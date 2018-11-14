022ab7eb1ec6c4508f77ba786d951b47362499f8
fw867 <fw867@163.com>
2018-11-14 16:58:20 +0800

    update message for 2.25

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,10 +1,11 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.24 r9105 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.25 r9243 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.14.75；<br />\
-<b>2. </b>优化fullcone支持；<br />\
-<b>3. </b>升级Intel微码、修复N3X系列重启的问题；<br />\
-<b>4. </b>增加一些网卡驱动；<br />",
+<b>1. </b>升级内核到4.14.80；<br />\
+<b>2. </b>增加nft-qos；<br />\
+<b>3. </b>增加wireguard；<br />\
+<b>4. </b>增加intel X710万兆驱动；<br />\
+<b>5. </b>修复一些bug；<br />",
 }
