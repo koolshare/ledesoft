4bdf566787affd9fae140b46ff19388f3784749f
houzi- <wojiaolinmu008@gmail.com>
2018-12-10 21:19:09 +0800

    Refine version

--- koolddns/config.json.js
+++ koolddns/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"0.2",
-"md5":"17b85a99c35463ee395ecfe209ad04d8",
+"version":"0.2.1",
+"md5":"b013a4fb62fbfdfa0419b2428fe4fad6",
 "home_url":"Module_koolddns.asp",
 "title":"Koolddns",
 "description":"动态域名解析工具",
 "changelog":"增加Secret/Token填写位数",
-"build_date":"2018-12-10_21:02:22"
+"build_date":"2018-12-10_21:18:13"
 }
