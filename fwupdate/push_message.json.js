f3fdf5fb6732f36ffd5f65b6790fc5bd5b04b151
fw867 <fw867@163.com>
2019-01-01 15:54:15 +0800

    update message for V2.26

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,11 +1,10 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.25 r9243 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.26 r9768 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
-<br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>",
+<br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>\
+<br/>祝大家元旦快乐！一笔“狗”销，“猪”事顺利！",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.14.80；<br />\
-<b>2. </b>增加nft-qos；<br />\
-<b>3. </b>增加wireguard；<br />\
-<b>4. </b>增加intel X710万兆驱动；<br />\
-<b>5. </b>修复一些bug；<br />",
+<b>1. </b>升级内核到4.14.90；<br />\
+<b>2. </b>增加docker支持；<br />\
+<b>3. </b>修复一些bug；<br />",
 }
