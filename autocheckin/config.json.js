fcee9591fcf26eb33d742d8612a1d39d0506304f
xiaobao <xiaobao@linkease.com>
2021-08-06 16:25:26 +0800

    change autocheck to modules.json

--- autocheckin/config.json.js
+++
@@ -1,9 +0,0 @@
-{
-"version":"2.0",
-"md5":"3d77bb60c4316f2dc457bc5cb02c6fc2",
-"home_url":"Module_autocheckin.asp",
-"title":"签到狗3.0",
-"description":"每日批量自动签到",
-"changelog":"修正更新",
-"build_date":"2021-02-08_23:55:15"
-}
